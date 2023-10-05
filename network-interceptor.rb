5d2372f4be0cf959357481844ab03f13196e29fa
gajkumar <gajkumar@expediagroup.com>
2023-10-05 23:49:48 +0530

    published_release

--- network-interceptor.rb
+++ network-interceptor.rb
@@ -3,17 +3,17 @@ class NetworkInterceptor < Formula
   homepage ""
   # Conditionally select the URL based on the architecture
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/macos-x64_8.0.0/network-interceptor.tar.gz"
-    sha256 "4b6805e408426182343ef3416c05cf63ea073e7fbe59df7309407f006fd82814"
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/macos-x64_9.0.0/network-interceptor.tar.gz"
+    sha256 "623ac34e159bb657eb5c0aae3e895d78ba6f5c9abd326092c5791e8e771979d1"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/macos-arm64_8.0.0/network-interceptor.tar.gz"
-    sha256 "c4b3808aba3ff6228ea6aac197faf102e9b3c393e3dba19b1670b37829d6febc"
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/macos-arm64_9.0.0/network-interceptor.tar.gz"
+    sha256 "92a48d6e1bcd81a0aa1aade473f3f4ae9acc7e4ea07da9ec485f72525bf320b7"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/linux-x64_8.0.0/network-interceptor.tar.gz"
-    sha256 "4b6805e408426182343ef3416c05cf63ea073e7fbe59df7309407f006fd82814"
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/linux-x64_9.0.0/network-interceptor.tar.gz"
+    sha256 "623ac34e159bb657eb5c0aae3e895d78ba6f5c9abd326092c5791e8e771979d1"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/linux-arm64_8.0.0/network-interceptor.tar.gz"
-    sha256 "00b7423e6c13bdef71f897e3e6fad17ff98f697df719f0ceec671fdb87fc4c5a"
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/linux-arm64_9.0.0/network-interceptor.tar.gz"
+    sha256 "a7f5324f2430124e437de662ae5c0a07d1f5390889a74e690c30c5ab3e1a0fb8"
   else
     # Code for unsupported platforms or architectures
   end
