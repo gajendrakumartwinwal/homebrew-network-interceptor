52f9e6a7da7e32a5650162e87057beac0e1845d9
gajkumar <gajkumar@expediagroup.com>
2023-10-06 11:01:20 +0530

    trying different download paths

--- network-interceptor.rb
+++ network-interceptor.rb
@@ -3,17 +3,17 @@ class NetworkInterceptor < Formula
   homepage ""
   # Conditionally select the URL based on the architecture
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/macos-x64_10.0.0/network-interceptor.tar.gz"
-    sha256 ""
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/12.0.0/macos-x64-network-interceptor.tar.gz"
+    sha256 "42403be37876f05eddac2f2ee2c1c5683cb1ce59fb98811ef9950866f274c5b7"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/macos-arm64_10.0.0/network-interceptor.tar.gz"
-    sha256 ""
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/12.0.0/macos-arm64-network-interceptor.tar.gz"
+    sha256 "4e2e8a95c13901e559a137dd1e8e0ad0a2f1532a427f99fdd49ef28372cc97fe"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/linux-x64_10.0.0/network-interceptor.tar.gz"
-    sha256 ""
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/12.0.0/linux-x64-network-interceptor.tar.gz"
+    sha256 "42403be37876f05eddac2f2ee2c1c5683cb1ce59fb98811ef9950866f274c5b7"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/linux-arm64_10.0.0/network-interceptor.tar.gz"
-    sha256 ""
+    url "https://github.com/gajendrakumartwinwal/homebrew-tap-network-interceptor/releases/download/12.0.0/linux-arm64-network-interceptor.tar.gz"
+    sha256 "6d893ca3c2ee9cddee0d13b6b82b5f048b306489af2a254d4d2e662e3ba51268"
   else
     # Code for unsupported platforms or architectures
   end
