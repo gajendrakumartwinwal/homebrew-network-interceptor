40c1c086c0c65132832a01b9c56e53dd1272bdaa
gajkumar <gajkumar@expediagroup.com>
2023-10-06 10:12:24 +0530

    testing

--- publish-releases.sh
+++ publish-releases.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "GAJENDRA"
+echo $SHELL
 
 # Define the version prefixes and version
 version_prefixes=("macos-x64" "macos-arm64" "linux-x64" "linux-arm64")
@@ -7,6 +9,6 @@ version=$(node -pe "require('./package.json').version")
 # Loop through the version prefixes
 for prefix in "${version_prefixes[@]}"; do
   # Run script2.sh with arguments
-  source publish-release.sh "${prefix}" "${version}"
-#  echo "${prefix}" "${version}"
+#  source publish-release.sh "${prefix}" "${version}"
+  echo "${prefix}" "${version}"
 done
